11106770be5d3b5b8c0bf2c1d6f8632b931e4e4c
AdenEarnshaw <github@adenearnshaw.com>
2020-05-06 09:28:21 +0100

    Adding Master Detail Sample

---
+++ MasterDetailPage/AppShortcutsMasterDetailTest/AppShortcutsMasterDetailTest/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppShortcutsMasterDetailTest.Services;
+using AppShortcutsMasterDetailTest.Views;
+using AppShortcutsMasterDetailTest.Models;
+using AppShortcutsMasterDetailTest.ViewModels;
+
+namespace AppShortcutsMasterDetailTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override async void OnAppLinkRequestReceived(Uri uri)
+        {
+            var itemId = uri.ToString().Replace("stc://masterdetailtest/items/", "");
+
+            if (!string.IsNullOrEmpty(itemId))
+            {
+                var item = await DependencyService.Get<IDataStore<Item>>().GetItemAsync(itemId);
+                await (MainPage as MasterDetailPage)?.Detail.Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            }
+            else
+            {
+                base.OnAppLinkRequestReceived(uri);
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MasterDetailPage/AppShortcutsMasterDetailTest/AppShortcutsMasterDetailTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+using AppShortcutsMasterDetailTest.Models;
+using Plugin.AppShortcuts;
+using Plugin.AppShortcuts.Icons;
+using Xamarin.Forms;
+
+namespace AppShortcutsMasterDetailTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ICommand AddShortcutCommand { get; set; }
+        
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+            AddShortcutCommand = new Command(AddShortcut);
+        }
+
+        private async void AddShortcut()
+        {
+            var sc = new Shortcut
+            {
+                Label = Item.Text,
+                Description = Item.Text,
+                Icon = new FavoriteIcon(),
+                Uri = $"stc://masterdetailtest/items/{Item.Id}"
+            };
+            await CrossAppShortcuts.Current.AddShortcut(sc);
+        }
+    }
+}
